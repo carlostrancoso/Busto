1c564a34da035a368ede5f517f414d6669cede33
carlostrancoso <90407115+carlostrancoso@users.noreply.github.com>
2021-12-18 17:44:48 +0000

    Update Busto_15.js

--- Busto_15.js
+++ Busto_15.js
@@ -7,36 +7,37 @@ function preload() {
 }
 
 function setup() {
-  var cnv = createCanvas(300, 450, WEBGL);
+  createCanvas(windowWidth, windowHeight, WEBGL);
+  //var cnv = createCanvas(300, 450, WEBGL);
   //debugMode();
   //background(7);
-  var x = (windowWidth - width) / 2;
-  var y = (windowHeight - height) / 2;
-  cnv.position(x, y);
+  //var x = (windowWidth - width) / 2;
+  //var y = (windowHeight - height) / 2;
+  //cnv.position(x, y);
 }
 
 function draw() {
-  //background(7);
+  background(255);
  
-  let locX = mouseX - width / 2;
-  let locY = mouseY - height / 2;
-  pointLight(250, 0, 0, locX, locY, width/2);
-  pointLight(0, 0, 250, locX*-1, locY*-1, height/2);
-  pointLight(2500, 250, 250, locX*1.2, locY*1.2, height/2);
+  //let locX = mouseX - width / 2;
+  //let locY = mouseY - height / 2;
+  pointLight(250, 0, 0, 0, 100, width/2);
+  pointLight(0, 0, 250, -100, -100, height/2);
+  pointLight(2500, 250, 250, 150, 150, height/2);
   //normalMaterial();
   texture(map);
-  orbitControl(5,5,0.5);
-  rotateX(PI/6);
+  orbitControl(5,5,0.2);
+  //rotateX(PI/6);
   //rotateY(-1);
-  rotateZ(PI);
+ // rotateZ(PI);
   noStroke();
   
-  for (let j = 0; j < 5; j++) {
+  for (let j = 0; j < 6; j+=3) {
     push();
-    for (let i = 0; i < 80; i++) {
+    for (let i = 0; i < 80; i+=5) {
       translate(
         sin(frameCount * 0.001 + j) * 100,
-        sin(frameCount * 0.001 + j) * 100,
+        sin(frameCount * 0.01 + j) * 100,
         i * 0.1
       );
       rotateZ(frameCount * 0.002);
